07a38b21e48a863e12e0f01e2d517c62141a4857
ayushi.pandey <ayushi.pandey@getmyuni.com>
2021-09-09 00:13:43 +0530

    api data

--- api/config/params.php
+++ api/config/params.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'weatherApiUrl' => 'api.openweathermap.org/data/2.5/weather',
+    'weatherApiUrl' => 'https://api.openweathermap.org',
     'weatherAppId' => '7ab489c603baca5efb72d3dfee043bd8'
 ];
--- api/controllers/WeatherController.php
+++ api/controllers/WeatherController.php
@@ -4,7 +4,9 @@ namespace api\controllers;
 
 use Yii;
 use yii\base\Module;
+use yii\helpers\ArrayHelper;
 use yii\web\Controller;
+use yii\httpclient\Client;
 /**
  * WeatherController.
  */
@@ -12,6 +14,7 @@ class WeatherController extends Controller
 {
     protected $baseUrl;
     protected $baseParams;
+    protected $client;
 
     function __construct($id, Module $module, array $config = [])
     {
@@ -19,12 +22,35 @@ class WeatherController extends Controller
         $this->baseParams = [
             "appid" => Yii::$app->params['weatherAppId'],
         ];
+        $this->client = new Client();
         parent::__construct($id, $module, $config);
     }
 
-    public function actionGetDetail()
+    public function actionGetDetail($q)
+    {   
+        $response = $this->getResponse($q);
+        print_r($response);exit();
+    }
+
+
+    protected function getResponse($query)
     {
-        print_r('hello');exit();
+        $url = $this->baseUrl . "/data/2.5/weather";
+        $data = [
+            'q' => $query
+        ];
+        $params = ArrayHelper::merge($this->baseParams, $data);
+        $response = $this->client->createRequest()
+        ->setMethod('GET')
+        ->setUrl($url)
+        ->setData($params)
+        ->send();
+        return $response;
+    }
+
+    protected function prepareResponse($response)
+    {
+
     }
 
 }
