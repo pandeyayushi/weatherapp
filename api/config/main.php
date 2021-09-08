829f5559f5208a7ad6a6ee7680f6c5d6c7838cbe
ayushi.pandey <ayushi.pandey@getmyuni.com>
2021-09-08 22:20:09 +0530

    first commit

---
+++ api/config/main.php
@@ -0,0 +1,80 @@
+<?php
+
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-api',
+    'name' => 'weather-api',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'api\controllers',
+    'modules' => [
+    ],
+    'components' => [
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'enableSession' => false,
+            'loginUrl' => null,
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'enableStrictParsing' => true,
+            'showScriptName' => false,
+            'rules' => [
+                [
+                'class' => 'yii\rest\UrlRule',
+                'controller' => 'weather',
+                'pluralize' => false,
+                'extraPatterns'=>[
+                    'GET weather-detail' => 'get-detail'
+                   ],
+               ],
+            ],
+        ],
+        'response' => [
+            'class' => \yii\web\Response::class,
+            'format' => yii\web\Response::FORMAT_JSON,
+            'charset' => 'UTF-8',
+            'on beforeSend' => function ($event) {
+                
+                $response = $event->sender;
+                if ($response->format == 'html') {
+                    return $response;
+                }
+
+                $responseData = $response->data;
+
+                if (is_string($responseData) && json_decode($responseData)) {
+                    $responseData = json_decode($responseData, true);
+                }
+
+                if ($response->statusCode >= 200 && $response->statusCode <= 299) {
+                    $response->data = [
+                        'success' => true,
+                        'status' => $response->statusCode,
+                        'data' => $responseData,
+                    ];
+                } else {
+                    $response->data = [
+                        'success' => false,
+                        'status' => $response->statusCode,
+                        'data' => $responseData,
+                    ];                    
+                }
+                return $response;
+            },
+        ],
+        'request' => [
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
+        ],
+    ],
+    'params' => $params,
+];
---
+++ api/controllers/WeatherController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace api\controllers;
+
+use yii\rest\ActiveController;
+
+/**
+ * WeatherController.
+ */
+class WeatherController extends ActiveController
+{
+
+}
